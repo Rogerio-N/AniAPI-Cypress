f29dda362505b3a4c9a419f579b101a655f8c5f8
Rogerio-N <rogenakayama@gmail.com>
2021-11-25 15:04:28 -0400

    FIX JWT case

--- cypress/integration/verify-jwt.spec.js
+++ cypress/integration/verify-jwt.spec.js
@@ -2,7 +2,7 @@
 
 context('Check if the JWT is valid', () => {
     it('Valid JWT', () => {
-        cy.verifyToken(Cypress.env("jwt")).then((response)=>{
+        cy.verifyToken(Cypress.env("JWT")).then((response)=>{
             expect(response.status).to.eq(200)
             expect(response.headers).to.be.an("object")
              .that.includes.all.keys("x-ratelimit-limit","x-ratelimit-remaining","x-ratelimit-reset")
@@ -11,7 +11,7 @@ context('Check if the JWT is valid', () => {
     });
 
     it('Invalid, JWT incorrect value', () => {
-        cy.verifyToken(Cypress.env("jwt")+1).then((response)=>{
+        cy.verifyToken(Cypress.env("JWT")+1).then((response)=>{
             expect(response.status).to.eq(401)
             expect(response.headers).to.be.an("object")
              .that.includes.all.keys("x-ratelimit-limit","x-ratelimit-remaining","x-ratelimit-reset")
